8c11c11043b8bf2378d4b5c4dbea9de49af480c4
Marcela Contrera <contrera.vet@gmail.com>
2025-02-22 19:17:46 -0300

    Implementa uma função para atualizar a lista de amigos.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 
 let grupoDeAmigos = []
 
+
+
 function adicionarAmigo() {
     let amigo = document.getElementById('amigo').value;
     //Validar a entrada: Implemente uma validação para garantir que o campo não esteja vazio.
@@ -12,7 +14,7 @@ function adicionarAmigo() {
     } 
 
     // Verifica se o nome já existe na lista de amigos.
-    if (grupoDeAmigos.includes(amigo)) {
+    if (grupoDeAmigos.includes(amigo.trim())) {
         alert("Este nome já foi adicionado. Por favor, insira um nome diferente.");
         return;
     }
@@ -20,6 +22,7 @@ function adicionarAmigo() {
     grupoDeAmigos.push(amigo.trim());
     console.log(grupoDeAmigos);
     // to be created     atualizarLista(); // Chama a função responsável por atualizar a exibição da lista de amigos na página.
+    atualizarListaDeAmigos()
     limparCampo();
 
 }
@@ -30,3 +33,16 @@ function limparCampo() {
     amigo.value = ''
 
 }
+
+// Crie uma função que percorra o array amigos e adicione cada nome como um elemento <li> dentro de uma lista HTML. Use innerHTML para limpar a lista antes de adicionar novos elementos.
+function atualizarListaDeAmigos() {
+    const listaAmigosElement = document.getElementById('listaAmigos'); // criei uma lista para o proposito de atualizar o HTML
+    listaAmigosElement.innerHTML = ''; // limpa o campo
+    grupoDeAmigos.forEach(amigo => {  // função de callback pra usar o método forEach, pega cada item da lista e itera com as ações abaixo
+        const li = document.createElement('li'); // cria um elemento no HTML para a lista ul <li>
+        li.textContent = amigo; // preenche essa li com o texto de um dos nomes do grupoDeAmigos
+        listaAmigosElement.appendChild(li); // atualiza o HTML com o elemento pronto.
+    });
+
+} 
+
